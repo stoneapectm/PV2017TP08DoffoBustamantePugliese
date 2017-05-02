b9c3f1160531ac291a6fdf62e29a3d182fad7044
Franco <Franco@Franco-PC>
2017-05-02 19:45:06 -0300

    Ratas subo la collecion libros

---
+++ src/java/aplicacion/datos/ColeccionLibros.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.datos;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Franco
+ */
+public class ColeccionLibros implements Serializable {
+    private ArrayList <Libro> libros;
+
+    public ColeccionLibros() {
+        libros=new ArrayList();
+    }
+
+    public void agregarLibros(Libro unLibro){
+        getLibros().add(unLibro);
+    }
+    /**
+     * @return the libros
+     */
+    public ArrayList <Libro> getLibros() {
+        return libros;
+    }
+
+    /**
+     * @param libros the libros to set
+     */
+    public void setLibros(ArrayList <Libro> libros) {
+        this.libros = libros;
+    }
+    
+}
