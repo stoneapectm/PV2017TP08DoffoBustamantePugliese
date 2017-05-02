bfad50378cad4e17cbb2f3f554fd8a89c69033a4
tienda <tienda@LAPTOP-I1O7PTCJ>
2017-05-02 19:45:26 -0300

    La clase libro

---
+++ src/java/aplicacion/modelo/dominio/Libro.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion.modelo.dominio;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author tienda
+ */
+public class Libro implements Serializable {
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    public Libro() {
+    }
+
+    public Libro(String isbn, String titulo, String autor, double precio) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+
+    /**
+     * @return the isbn
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * @param autor the autor to set
+     */
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
